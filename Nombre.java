74c023714f401d2585ac259aa663bb02efed96e0
JoseimyEmmanu <emmanuelbasulto4@gmail.com>
2024-03-01 15:19:08 -0600

    edite el codigo

--- Nombre.java
+++ Nombre.java
@@ -1,5 +1,6 @@
 public class Nombre{
         public static void main(String[]args){
                 System.out.println("Adair Emanuel Uc Chim");
+                System.out.println("joseimy Emmanuel Basulto Cituk");
         }
 }
