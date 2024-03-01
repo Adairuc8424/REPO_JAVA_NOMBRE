48f63332d65f86354d447910864d08965cc96a19
ArlanEnrique <8379itescam.edu.mx>
2024-03-01 15:25:14 -0600

    edite el archivo java y agregue mi nombre

--- Nombre.java
+++ Nombre.java
@@ -2,5 +2,7 @@ public class Nombre{
         public static void main(String[]args){
                 System.out.println("Adair Emanuel Uc Chim");
                 System.out.println("joseimy Emmanuel Basulto Cituk");
+                /*Editado por Arlan */
+                System.out.println("Arlan Enrique Suaste Cauich");
         }
 }
